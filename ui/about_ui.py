d7659f6f7cda76a0c8a6a70d5134fadd5e8c303d
Alex Yatskov <alex@foosoft.net>
2011-08-28 16:12:48 -0700

    version bump

--- ui/about_ui.py
+++ ui/about_ui.py
@@ -2,34 +2,48 @@
 
 # Form implementation generated from reading ui file 'dev/ui/about.ui'
 #
-# Created: Wed Sep  1 23:56:00 2010
-#      by: PyQt4 UI code generator 4.7.4
+# Created: Sun Aug 28 16:12:10 2011
+#      by: PyQt4 UI code generator 4.8.5
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
 class Ui_DialogAbout(object):
     def setupUi(self, DialogAbout):
-        DialogAbout.setObjectName("DialogAbout")
+        DialogAbout.setObjectName(_fromUtf8("DialogAbout"))
         DialogAbout.resize(470, 200)
+        DialogAbout.setWindowTitle(QtGui.QApplication.translate("DialogAbout", "About", None, QtGui.QApplication.UnicodeUTF8))
         self.horizontalLayout = QtGui.QHBoxLayout(DialogAbout)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setSizeConstraint(QtGui.QLayout.SetFixedSize)
         self.horizontalLayout.setMargin(0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.label = QtGui.QLabel(DialogAbout)
-        self.label.setPixmap(QtGui.QPixmap(":/img/img/banner_about.png"))
-        self.label.setObjectName("label")
+        self.label.setPixmap(QtGui.QPixmap(_fromUtf8(":/img/img/banner_about.png")))
+        self.label.setObjectName(_fromUtf8("label"))
         self.horizontalLayout.addWidget(self.label)
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setMargin(9)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.labelReadme = QtGui.QLabel(DialogAbout)
         self.labelReadme.setMinimumSize(QtCore.QSize(350, 0))
+        self.labelReadme.setText(QtGui.QApplication.translate("DialogAbout", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Mangle</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Version 2.2</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Manga processor by Alex Yatskov for the Kindle e-book reader. Please see <span style=\" font-style:italic;\">license.txt</span> for licensing information. Visit the homepage at <a href=\"http://foosoft.net/mangle\"><span style=\" text-decoration: underline; color:#0000ff;\">http://foosoft.net/mangle</span></a>.</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
         self.labelReadme.setWordWrap(True)
         self.labelReadme.setOpenExternalLinks(True)
-        self.labelReadme.setObjectName("labelReadme")
+        self.labelReadme.setObjectName(_fromUtf8("labelReadme"))
         self.verticalLayout.addWidget(self.labelReadme)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.verticalLayout.addItem(spacerItem)
@@ -42,23 +56,15 @@ class Ui_DialogAbout(object):
         sizePolicy.setHeightForWidth(self.buttonBox.sizePolicy().hasHeightForWidth())
         self.buttonBox.setSizePolicy(sizePolicy)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
+        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
         self.verticalLayout.addWidget(self.buttonBox)
         self.horizontalLayout.addLayout(self.verticalLayout)
 
         self.retranslateUi(DialogAbout)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), DialogAbout.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), DialogAbout.accept)
         QtCore.QMetaObject.connectSlotsByName(DialogAbout)
 
     def retranslateUi(self, DialogAbout):
-        DialogAbout.setWindowTitle(QtGui.QApplication.translate("DialogAbout", "About", None, QtGui.QApplication.UnicodeUTF8))
-        self.labelReadme.setText(QtGui.QApplication.translate("DialogAbout", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Mangle</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Version 2.1</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Manga processor by Alex Yatskov for the Kindle e-book reader. Please see <span style=\" font-style:italic;\">license.txt</span> for licensing information. Visit the homepage at <a href=\"http://foosoft.net/mangle\"><span style=\" text-decoration: underline; color:#0000ff;\">http://foosoft.net/mangle</span></a>.</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        pass
 
 import resources_rc
