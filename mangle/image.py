cee954068c0843d3de044e2e247d2962e394212b
Jan Martin <jan.ramon.martin@gmail.com>
2015-09-29 22:38:47 +1000

    kindle paperwhite 3 resolution added

--- mangle/image.py
+++ mangle/image.py
@@ -84,8 +84,9 @@ class KindleData:
         'Kindle 5': ((600, 800), Palette15b),
         'Kindle DX': ((824, 1200), Palette15a),
         'Kindle DXG': ((824, 1200), Palette15a),
-        'Kindle Touch': ((600, 800), Palette15a), 
+        'Kindle Touch': ((600, 800), Palette15a),
         'Kindle Paperwhite': ((758, 1024), Palette15b), # resolution given in manual, see http://kindle.s3.amazonaws.com/Kindle_Paperwhite_Users_Guide.pdf
+        'Kindle Paperwhite 3': ((1072, 1448), Palette15b),
         'KoBo Aura H2o': ((1080, 1430), Palette15a), # resolution from http://www.fnac.com/Liseuse-Numerique-Kobo-by-Fnac-Kobo-Aura-H2O-Noir/a7745120/w-4
     }
 
